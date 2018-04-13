f786f5c5f56ac23b5b414107fcdd49d284355882
sairic <ricardo@sairic-consulting.com>
2018-04-12 21:53:19 -0400

    Completed

--- src/test/kotlin/com/sairic/example/kotlinstandardfunctions/KotlinStandardFunctionsTest.kt
+++ src/test/kotlin/com/sairic/example/kotlinstandardfunctions/KotlinStandardFunctionsTest.kt
@@ -12,6 +12,8 @@ import org.junit.jupiter.api.Assertions.assertTrue
 
 class KotlinStandardFunctionsTest {
 
+    private val someClassVariable = "Class Level Variable"
+
     /**
      * Shows that 'run' by default will pass in a reference to this. It can be used implicitly or
      * explicity use 'this'
@@ -55,16 +57,66 @@ class KotlinStandardFunctionsTest {
         bimmer.let {
             assert(it.horsePower == 425)
             it.isAccelerating = true
+
+            //Will print 'Class Level Variable'
+            println(this.someClassVariable)
         }
         assert(bimmer.isAccelerating)
-
 
         bimmer.let { myawesomebimmer ->
             assert(myawesomebimmer.horsePower == 425)
         }
         assert(bimmer.isAccelerating)
 
+
+    }
+
+    @Test
+    @DisplayName("Show 'also' and how it always returns itself(this)")
+    fun testAlso() {
+        val bimmer = Bimmer(2018, "X5M", 560, 101700.00, false)
+
+        bimmer.also { mybimmer ->
+            mybimmer.horsePower = 567
+        }.also { mybimmer ->
+            mybimmer.isAccelerating = true
+        }
+
+        assert(bimmer.horsePower == 567)
+        assert(bimmer.isAccelerating)
+    }
+
+    @Test
+    @DisplayName("Show that 'apply' always passes in and returns itself(this)")
+    fun testApply() {
+        val bimmer = Bimmer(2017, "i8", 357, 143000.00, false)
+
+        val bimmerCopy = bimmer.apply {
+            price = 148000.00
+            this.isAccelerating = true
+        }
+
+        assert(bimmerCopy.price == 148000.00)
+        assert(bimmerCopy.isAccelerating)
+        assert(bimmer.price == 148000.00)
+    }
+
+    @Test
+    @DisplayName("Show that 'apply' with chaining")
+    fun testApplyWithChaining() {
+        val bimmer = Bimmer(2017, "i8", 357, 143000.00, false)
+
+        val bimmerCopy = bimmer.apply {
+            price = 148000.00
+        }.apply {
+            this.isAccelerating = true
+        }
+
+        assert(bimmerCopy.price == 148000.00)
+        assert(bimmerCopy.isAccelerating)
+        assert(bimmer.price == 148000.00)
     }
 
 
 }
+
